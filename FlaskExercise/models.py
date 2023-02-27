186d757b49439d32011f8b6cdb9c3234093d1553
tuyennguyen99 <nguyentuyen95.bt@gmail.com>
2023-02-28 00:12:17 +0700

    Update upload and delete blob

--- FlaskExercise/models.py
+++ FlaskExercise/models.py
@@ -2,12 +2,13 @@ from FlaskExercise import app, db
 from flask import flash
 from werkzeug.utils import secure_filename
 from azure.storage.blob import BlobServiceClient
+
 import uuid
 
 blob_container = app.config['BLOB_CONTAINER']
 storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
 blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['BLOB_STORAGE_KEY'])
-
+container_client = ContainerClient(account_url=storage_url, container_name=app.config['BLOB_CONTAINER'], credential=app.config['BLOB_STORAGE_KEY'])
 class Animal(db.Model):
     __tablename__ = 'animals'
     id = db.Column(db.Integer, primary_key=True)
@@ -26,10 +27,16 @@ class Animal(db.Model):
             randomFilename = str(uuid.uuid1())
             filename = randomFilename + '.' + fileExtension
             try:
-                # TODO: Get a blob client and upload the blob
+                blob_container = app.config['BLOB_CONTAINER']
+                storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
+                blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['BLOB_STORAGE_KEY'])
+
+                blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
+                blob_client.upload_blob(file)
                 pass
                 if self.image_path:
-                    # TODO: Get a blob client and delete the previous blob
+                    blob_client = blob_service.get_blob_client(container=blob_container, blob=self.image_path)
+                    blob_client.delete_blob()
                     pass
             except Exception as err:
                 flash(err)
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import quote
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -6,11 +7,11 @@ class Config(object):
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'hello-world-server.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'hellow-world-db'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin1234'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '@1995N89y1234'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or quote('@1995N89y12345')
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    logging.error(SQLALCHEMY_DATABASE_URI)
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'udacitylearning1234'
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'USkJOFLzmHAzHP06ek5g2GgO81kfUqJmlt4Bb3gy7S6LqXqTdSpIDdVowcNTElcmL6ZUlV7Zhprn+AStKzMNSQ=='
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
